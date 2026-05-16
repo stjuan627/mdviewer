c2f80667676630a8dfdde98ada73818b9044c04b
James Wang <stjuan627@gmail.com>
2026-05-17 01:00:18 +0800

    chore: hide markdown-viewer

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
   output: 'static',
+  site: 'https://mdviewer.net',
   adapter: cloudflare(),
   integrations: [react()],
   vite: {
